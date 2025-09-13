e4dfcf38e64d5d68aee486e1a1519c8667374b21
ceralusjinel-web <ceralusjinel@gmail.com>
2025-09-12 23:39:11 -0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~JyVQ2LSY#Wt8gsXFqWoI8lv8ZaR8QTNOxIJHf9o-zGBvDBkAIgCE",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50935623320", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "a", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "France" //put your country timeZone....leave blank if u don't know.
 };
