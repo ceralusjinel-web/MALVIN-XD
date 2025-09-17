e41f30dc4e57d322cd5fa57f85a387603ea9a945
ceralusjinel-web <ceralusjinel@gmail.com>
2025-09-16 20:49:13 -0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Y69UWIwS#Mirq4hropgGPYtMFEP6-ofBG5h6qyYbPTUGsFiy0gis",  // add your Session Id here
+  SESSION_ID: "malvin~07dU1JSY#wrFyETzc5ibzVizCRCsT7q7rgbOeBWi8d_KqH92-wx4",  // add your Session Id here
   
   OWNER_NUMBER: "18299257701", // put your phone number here
   
